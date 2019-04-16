b20e5d2dffa166953c23a4476cf482ee8344a54a
John Raptis <johnraptis@Christoss-MBP-2.fritz.box>
2019-04-16 15:00:51 +0300

    second

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Musaic', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
+  siteTitleAlt: 'Musaic', // Alternative Site title for SEO
   siteTitleManifest: 'MinimalBlog',
   siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
